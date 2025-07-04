1934d8384b1a95c3f4e91026aa61a315d26e61e8
Akbar <konokovakbar@gmail.com>
2025-07-04 18:53:55 +0600

    First commit

---
+++ урок_9.py
@@ -0,0 +1,10 @@
+class Car:
+    # инициализатор
+    def __init__(self, model, year):
+         # свойства атрибуты поля
+         self.model = model
+         self.year = year
+         self.fined = False
+
+
+print("s")
