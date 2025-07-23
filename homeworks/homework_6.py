f7e8ea2072019b1f996cc4146ba454e824327a70
Akbar <konokovakbar@gmail.com>
2025-07-23 19:53:31 +0600

    домашние работы

---
+++ homeworks/homework_6.py
@@ -0,0 +1,15 @@
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+RESET = "\033[0m"
+
+print(RED + "apple" + RESET)
+print(GREEN + "banana" + RESET)
+print(YELLOW + "cherry" + RESET)
+print(BLUE + "grape" + RESET)
+print(MAGENTA + "mango" + RESET)
+print(CYAN + "orange" + RESET)
+print(RESET + "peach" + RESET)
