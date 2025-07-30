36b1e3267b96603513ff556f469e589ae52da980
Akbar <konokovakbar@gmail.com>
2025-07-30 08:32:32 +0600

    исправил дз 6

--- homeworks/homework_6.py
+++ homeworks/homework_6.py
@@ -1,23 +1,9 @@
-colors = {
-    "red": "\033[31m",
-    "yellow": "\033[33m",
-    "orange": "\033[38;5;208m",
-    "purple": "\033[35m",
-    "gold": "\033[38;5;220m",
-    "blue": "\033[34m",
-    "green": "\033[32m",
-    "reset": "\033[0m"
-}
+from blessed import Terminal
 
-fruits = [
-    ("Apple", "red"),
-    ("Banana", "yellow"),
-    ("Orange", "orange"),
-    ("Grapes", "purple"),
-    ("Mango", "gold"),
-    ("Blueberry", "blue"),
-    ("Kiwi", "green")
-]
+term = Terminal()
 
-for fruit, color in fruits:
-    print(colors[color] + fruit + colors["reset"])
+print(term.red('apple'))
+print(term.yellow('Banana'))
+print(term.magenta('Grapes'))
+print(term.blue('Blueberry'))
+print(term.green('Kiwi'))
